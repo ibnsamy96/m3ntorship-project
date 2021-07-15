e3a60639287b0759a113072ee0db8bb75a24b5dc
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-07-15 14:57:09 +0200

    updated the shared cta-btn to support responsivness

--- src/app/shared/cta-btn/cta-btn.component.ts
+++ src/app/shared/cta-btn/cta-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cta-btn',
@@ -12,8 +12,49 @@ export class CtaBtnComponent implements OnInit {
   @Input() fontSize!: string;
   @Input() paddingX!: string;
   @Input() paddingY!: string;
+  @Input() responsivenessBehavior!: {
+    breakpoint: number;
+    fontSize?: string;
+    paddingX?: string;
+    paddingY?: string;
+  }[];
 
+  finalState!: {
+    paddingY: string;
+    paddingX: string;
+    fontSize: string;
+    lineHeight: string;
+    letterSpacing: string;
+    buttonText: string;
+  };
+
+  @HostListener('window:resize', [])
+  private onResize() {
+    this.detectScreenSize();
+  }
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.finalState = {
+      buttonText: this.buttonText,
+      letterSpacing: this.letterSpacing,
+      lineHeight: this.lineHeight,
+      fontSize: this.fontSize,
+      paddingX: this.paddingX,
+      paddingY: this.paddingY,
+    };
+
+    this.detectScreenSize();
+  }
+
+  detectScreenSize(): void {
+    const innerWidth = window.innerWidth;
+    this.responsivenessBehavior?.forEach((state) => {
+      if (innerWidth <= state.breakpoint) {
+        this.finalState.fontSize = state.fontSize ?? this.fontSize;
+        this.finalState.paddingX = state.paddingX ?? this.paddingX;
+        this.finalState.paddingY = state.paddingY ?? this.paddingY;
+      }
+    });
+  }
 }
